f707990cd1690f38ec166d9023aabd4adfa1ea32
Filipe <ribeiro.filipe94@gmail.com>
2020-06-04 01:37:45 -0300

    feat: first part backend finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333);
